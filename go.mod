e9a0f084784ea44a92550399c096fa1f4861b94b
Dhruv Thaker <dhruvthaker777@gmail.com>
2026-01-10 21:05:21 +0530

    finally getting started

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dhruv/probe
+
+go 1.24.5
---
+++ hello.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type Urls struct {
+	url string
+}
+
+type UrlQueue struct {
+	UrlsToPoll chan *Urls
+}
+
+func main() {
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	const workers = 3
+	wg.Add(workers)
+
+	Urls := []*Urls{
+		{url: "https://google.com"},
+		{url: "https://github.com"},
+		{url: "https://stackoverflow.com"},
+		{url: "https://reddit.com"},
+		{url: "https://news.ycombinator.com"},
+		{url: "https://golang.org"},
+		{url: "https://medium.com"},
+		{url: "https://twitter.com"},
+		{url: "roadmap.sh/golang"},
+		{url: "https://linkedin.com"},
+	}
+
+	urlq := NewUrlQueue()
+	go urlq.EnqueueUrls(ctx, &wg, Urls)
+
+	for i := 1; i <= workers; i++ {
+		go urlq.DeQueueUrls(ctx, &wg)
+	}
+
+	wg.Wait()
+}
+
+func NewUrlQueue() *UrlQueue {
+	return &UrlQueue{
+		UrlsToPoll: make(chan *Urls),
+	}
+}
+
+func (uq *UrlQueue) EnqueueUrls(ctx context.Context, wg *sync.WaitGroup, urls []*Urls) {
+	defer wg.Done()
+
+	for _, u := range urls {
+		select {
+		case uq.UrlsToPoll <- u:
+			fmt.Println("url Enqueued")
+
+		case <-ctx.Done():
+			fmt.Printf("oops: %v\n", ctx.Err())
+			return
+		}
+	}
+	close(uq.UrlsToPoll)
+}
+
+func (uq *UrlQueue) DeQueueUrls(ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case url, ok := <-uq.UrlsToPoll:
+			if !ok {
+				return
+			}
+
+			req, _ := http.NewRequestWithContext(ctx, http.MethodGet, url.url, nil)
+			resp, err := http.DefaultClient.Do(req)
+
+			if err != nil {
+				fmt.Println("error with the url", err)
+				continue
+			}
+
+			fmt.Println("response:", resp)
+			resp.Body.Close()
+
+		case <-ctx.Done():
+			fmt.Printf("oops: %v\n", ctx.Err())
+			return
+		}
+	}
+
+}
+
+//func GoRoutineEg(ctx context.Context, wg *sync.WaitGroup, ch chan string) {
+//defer wg.Done()
+
+//select {
+//case ch <- "hello Go Routines":
+//fmt.Printf("sent to the channel")
+//case <-ctx.Done():
+//fmt.Println("oops", ctx.Err())
+//}
+//close(ch)
+//}
+
+//func ReceivFromChannel(ctx context.Context, wg *sync.WaitGroup, ch chan string) {
+//defer wg.Done()
+
+//select {
+//case mssg := <-ch:
+//fmt.Println("received mssg from channel :", mssg)
+//case <-ctx.Done():
+//fmt.Println("oops", ctx.Err())
+//}
+
+//}
